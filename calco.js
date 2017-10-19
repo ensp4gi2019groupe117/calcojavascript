9d77ec6f54b188f09660c0086035af76ee8b363f
Sim'S <sfaysamaritan@gmail.com>
2017-10-19 23:18:20 +0100

    Page avec HTML CSS Debut JavaScript

---
+++ calco.js
@@ -0,0 +1,67 @@
+
+var op1 = "";
+var op = "";
+var op2 = "";
+var result = 0;
+
+
+var screen = document.getElementById("calc");
+//var log = document.getElementById("log");
+
+var saisie = function(key){
+	screen.value = screen.value + key;
+	if (op==""){
+		op1 = screen.value;
+	}
+	else{
+		op2 = screen.value;
+	}
+	//setLog();
+};
+
+var operation = function(key){
+	//alert(op1 + " op:" + op + " op2:" + op2);
+	if (op==""){
+		if (op1==""){
+			if (key=="-"){
+				screen.value = "-";
+				op1 = screen.value;
+			}
+			else if (key!="+"){
+				alert("Erreur!!! Entrez un nombre");
+			}
+		}
+		else{
+			op = key;
+			screen.value = "";
+		}
+	}
+	else{
+		if (key=="-"){
+			screen.value = "-";
+			op2 = screen.value;
+		}
+		else if (key!="+"){
+			alert("Erreur!!! Entrez un nombre");
+		}
+	}
+	//setLog();
+};
+
+var egal = function(){
+	screen.value = "";
+	if (op1!="" && op!="" && op2!="") calcul();
+};
+
+/*var setLog = function(){
+	var logval = "OP1: "+op1+", OP: "+op+", OP2: "+op2+".";
+	log.value = logval;
+};*/
+
+var calcul = function(){
+	screen.value = eval(op1 + "" + op + "" + op2);
+	op1 = ""+screen.value;
+	op = "";
+	op2 = "";
+};
+
